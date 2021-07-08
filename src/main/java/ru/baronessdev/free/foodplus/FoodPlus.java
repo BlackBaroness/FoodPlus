d7b46d5dc973c4019135d985c604185f68c40332
BlackBaroness <shoyurash2@gmail.com>
2021-07-08 03:27:37 +0300

    added our common-lib

--- src/main/java/ru/baronessdev/free/foodplus/FoodPlus.java
+++ src/main/java/ru/baronessdev/free/foodplus/FoodPlus.java
@@ -2,28 +2,30 @@ package ru.baronessdev.free.foodplus;
 
 import org.bstats.bukkit.Metrics;
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
-import ru.baronessdev.free.foodplus.util.UpdateCheckerUtil;
-import ru.baronessdev.free.foodplus.util.logging.LogType;
-import ru.baronessdev.free.foodplus.util.logging.Logger;
+import ru.baronessdev.lib.common.log.LogLevel;
+import ru.baronessdev.lib.common.log.Logger;
+import ru.baronessdev.lib.common.update.UpdateCheckerUtil;
 
 import java.util.List;
 
 public final class FoodPlus extends JavaPlugin {
 
+    private Logger logger;
+
     @Override
     public void onEnable() {
         saveDefaultConfig();
         new Metrics(this, 11263);
+        logger = new Logger.Builder("[FoodPlus]").build();
         load();
         checkUpdates();
     }
 
     private void load() {
-        Logger.log(LogType.INFO, "Loading...");
+        logger.log(LogLevel.INFO, "Loading...");
         FileConfiguration cfg = getConfig();
 
         int loaded = 0;
@@ -32,7 +34,7 @@ public final class FoodPlus extends JavaPlugin {
             String maybeMaterial = cfg.getString(id + ".material");
             Material material = Material.getMaterial(maybeMaterial);
             if (material == null) {
-                Logger.log(LogType.ERROR, "Can't get material (" + maybeMaterial + ")! Handler with id " + id + " is passing.");
+                logger.log(LogLevel.ERROR, "Can't get material (" + maybeMaterial + ")! Handler with id " + id + " is passing.");
                 continue;
             }
 
@@ -40,7 +42,7 @@ public final class FoodPlus extends JavaPlugin {
 
             List<String> execute = cfg.getStringList(id + ".execute");
             if (execute.isEmpty()) {
-                Logger.log(LogType.ERROR, "Empty execute query! Handler with id " + id + " is passing.");
+                logger.log(LogLevel.ERROR, "Empty execute query! Handler with id " + id + " is passing.");
                 continue;
             }
 
@@ -48,19 +50,10 @@ public final class FoodPlus extends JavaPlugin {
             loaded++;
         }
 
-        Logger.log(LogType.INFO, loaded + " listeners successfully activated.");
+        logger.log(LogLevel.INFO, loaded + " listeners successfully activated.");
     }
 
     private void checkUpdates() {
-        try {
-            int i = UpdateCheckerUtil.check(this);
-            if (i != -1) {
-                Logger.log(LogType.INFO, "New version found: v" + ChatColor.YELLOW + i + ChatColor.GRAY + " (Current: v" + getDescription().getVersion() + ")");
-                Logger.log(LogType.INFO, "Update now: " + ChatColor.AQUA + "market.baronessdev.ru/shop/licenses/");
-            }
-        } catch (UpdateCheckerUtil.UpdateCheckException e) {
-            Logger.log(LogType.ERROR, "Could not check for updates: " + e.getRootCause());
-            Logger.log(LogType.ERROR, "Please contact Baroness's Dev if this isn't your mistake.");
-        }
+        UpdateCheckerUtil.checkAsynchronously(this, "https://market.baronessdev.ru/shop/foodplus.4/", logger);
     }
 }
--- src/main/java/ru/baronessdev/free/foodplus/util/logging/LogType.java
+++
@@ -1,7 +0,0 @@
-package ru.baronessdev.free.foodplus.util.logging;
-
-public enum LogType {
-
-    INFO,
-    ERROR
-}
